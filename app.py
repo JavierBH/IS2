d47dc8167e601897ace34defdabbb4bbedf69806
OnAaron <garcia.muniz.aaron@gmail.com>
2019-11-12 12:03:31 +0100

    changes

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import sqlite3
 from flask import Flask, redirect, url_for, render_template, request, flash
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
+import mimetypes
 
+MY_ADDRESS = "norao97@gmail.com"
+PASSWORD = "ratillas"
 app = Flask(__name__)
 app.secret_key = 'random string'
     
@@ -42,15 +48,75 @@ def login():
     if request.method == "POST":
         conexion = conectar_db()
         cursor = conexion.cursor()
-        cursor.execute("SELECT password FROM Users WHERE name = ? AND password=?",(request.form["username"],request.form["password"]))
+        cursor.execute("SELECT password FROM Users WHERE usuario = ? AND password=?",(request.form["username"],request.form["password"]))
         rows = cursor.fetchone()
         if rows is not None:
-            flash("You are logged. Welcome !!!!")
+            flash("You are logged. Welcome !!!!","success")
             return redirect(url_for("home"))
+        else:
+            flash("Error in login. Check credentials","error")
+            #return redirect(url_for("login.html"))
     return render_template("login.html")
 
-"""@app.route("/recuperar", methods = ["GET","POST"])
-def recuperar():"""
+@app.route("/recuperar", methods = ["GET","POST"])
+def recuperar():
+    if request.method == "POST":
+        conexion = conectar_db()
+        cursor = conexion.cursor()
+        cursor.execute("SELECT usuario FROM Users WHERE email = ?",(request.form["correo"],))
+        rows = cursor.fetchone()
+        if rows is not None:
+            if enviar_correo(request.form["correo"]) == 0:
+                flash("E-mail sent. Look at your mail","success")
+                return redirect(url_for("new_pass"))
+        else:
+            flash("Email not valid. Try again","error")
+    return render_template("recuperar.html")
+
+@app.route("/recuperar/password", methods = ["GET","POST"])
+def new_pass():
+    if request.method == "POST":
+        conexion = conectar_db()
+        cursor = conexion.cursor()
+        cursor.execute("SELECT usuario FROM Users WHERE usuario = ?",(request.form["username"],))
+        rows = cursor.fetchone()
+        if rows is not None:
+            print(request.form["password"])
+            print(request.form["username"])
+            cursor.execute("UPDATE Users SET password=? WHERE usuario=?",(request.form["password"],request.form["username"]))
+            conexion.commit()
+            flash("Password change with success","success")
+            return redirect(url_for("login"))
+        else:
+            flash("Username not valid. Try again","error")
+    return render_template("new_pass.html")
+
+def enviar_correo(correo):
+    try:
+        destino = correo
+        #message = "Hello, world!"
+        message =  "http://127.0.0.1:5000/recuperar/password"
+        
+        s = smtplib.SMTP("smtp.gmail.com",587)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login(MY_ADDRESS,PASSWORD)
+
+        mime_message = MIMEText(message, "plain")
+        mime_message["From"] = MY_ADDRESS
+        mime_message["To"] = destino
+        mime_message["Subject"] = "Correo de prueba"
+        #mime_message.attach(MIMEText(message, 'plain'))
+        
+        s.send_message(mime_message)
+        del mime_message
+        s.quit()
+    except Exception:
+        return -1
+    return 0
+
+    #return render_template("recuperar.html")
 
 
 def conectar_db():
