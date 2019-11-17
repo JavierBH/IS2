6fb01d89b8b63fa775b2a682e874c00c9d8b2ddb
hanawang98 <hana.wang98>
2019-11-17 20:59:52 +0100

    Corregido comprobacion de correo

--- app.py
+++ app.py
@@ -72,9 +72,8 @@ def register():
             flash("Introduzca una contraseña de minimo 6 caracteres","error")
             return render_template("register.html")
         #COMPROBACION DE FORMATO CORREO
-        expresion = "\w+(@)([a-z]+).([a-z]+)"
-        tupla = re.match(expresion,email)
-        if tupla.group(1) != "@" or tupla.group(2) not in format_email or tupla.group(3) not in format_end:
+        expresion = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        if not(re.search(expresion, email)):
             flash("Email not valid","error")
             return render_template("register.html")
         #COMPROBACION DE CONTRASEÑA
