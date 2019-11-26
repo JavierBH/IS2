6e18f13af2c900f87d42b0d04184670ae76810b3
Javitemek <javierbarraganh@gmail.com>
2019-11-26 12:08:48 +0100

    Added toggled menus and search bar

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+
+function tab_main(evt,pageName) {
+    var i, tabcontent;
+    tabcontent = document.getElementsByClassName("main");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    document.getElementById(pageName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+  }
+
+  function tab_info_menu(evt,pageName) {
+    var i, tabcontent;
+    tabcontent = document.getElementsByClassName("info_menu");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    document.getElementById(pageName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+  }
+  document.getElementById("defaultOpen").click();
