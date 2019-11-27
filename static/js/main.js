90249a8cd3b4fb2d5d0e58a53923d40d407f548d
JavierBH <javierbarraganh@gmail.com>
2019-11-27 18:02:38 +0100

    Added help modal box

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,11 @@
 
+/*---------------CAMBIAR CONTENIDO CON PESTAÑAS -------*/
+
+document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpenSide").click();
+/*----------------------MENUS DE LOCALES Y DEGUSTACIONES --------- */
 function tab_main(evt,pageName) {
+
     var i, tabcontent;
     tabcontent = document.getElementsByClassName("main");
     for (i = 0; i < tabcontent.length; i++) {
@@ -10,6 +16,7 @@ function tab_main(evt,pageName) {
 
   }
 
+/*-------------MENUS DE USUARIO, ESTADISTICAS Y SOLICITUDES---------------- */
   function tab_info_menu(evt,pageName) {
     var i, tabcontent;
     tabcontent = document.getElementsByClassName("info_menu");
@@ -20,5 +27,30 @@ function tab_main(evt,pageName) {
     evt.currentTarget.className += " active";
 
   }
-  
-  document.getElementById("defaultOpen").click();
+
+  /*------------MODAL BOX DE AYUDA---------------- */
+// Get the modal
+var modal = document.getElementById("helpModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("helpBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
